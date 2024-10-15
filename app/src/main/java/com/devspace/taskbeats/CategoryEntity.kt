ee3481022dce0174d84b9b42610de8093b9aed6f
Task View <maumauriciog@hotmail.com>
2024-10-15 13:49:16 -0300

    Create Entity for Room Database

--- app/src/main/java/com/devspace/taskbeats/CategoryEntity.kt
+++ app/src/main/java/com/devspace/taskbeats/CategoryEntity.kt
@@ -5,7 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class CategoryEntity(
+data class CategoryEntity (
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     @ColumnInfo("key")
