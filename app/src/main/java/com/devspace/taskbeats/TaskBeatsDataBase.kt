ad323628607cdc999d81855db766ea1382455dd6
Task View <maumauriciog@hotmail.com>
2024-10-15 15:59:59 -0300

    Update DataBase for Room Database

--- app/src/main/java/com/devspace/taskbeats/TaskBeatsDataBase.kt
+++ app/src/main/java/com/devspace/taskbeats/TaskBeatsDataBase.kt
@@ -1,9 +1,10 @@
 package com.devspace.taskbeats
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database([CategoryEntity::class], version = 1)
-abstract class TaskBeatsDataBase{
+abstract class TaskBeatsDataBase : RoomDatabase(){
 
     abstract fun getCategoryDao(): CategoryDAO
 }
