6a131609f9c6f0ab8036844a4b29bf0ad7ed00c1
Mauricio Gomes das Chagas <maumauriciog@hotmail.com>
2024-10-22 17:51:24 -0300

    update TaskBeatsDataBase, include TaskEntity...

--- app/src/main/java/com/devspace/taskbeats/TaskBeatsDataBase.kt
+++ app/src/main/java/com/devspace/taskbeats/TaskBeatsDataBase.kt
@@ -3,7 +3,7 @@ package com.devspace.taskbeats
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database([CategoryEntity::class], version = 1)
+@Database([CategoryEntity::class, TaskEntity::class], version = 1)
 abstract class TaskBeatsDataBase: RoomDatabase() {
 
     abstract fun getCategoryDao(): CategoryDAO
