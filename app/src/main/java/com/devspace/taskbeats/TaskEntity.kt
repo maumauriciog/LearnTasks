28c1d9f12802338d28431c1ac9c97bbf87484006
Mauricio Gomes das Chagas <maumauriciog@hotmail.com>
2024-10-22 17:53:45 -0300

    update entity TaskEntity v2

--- app/src/main/java/com/devspace/taskbeats/TaskEntity.kt
+++ app/src/main/java/com/devspace/taskbeats/TaskEntity.kt
@@ -2,9 +2,11 @@ package com.devspace.taskbeats
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class TaskEntity(
+    @PrimaryKey
     @ColumnInfo("Name_category")
     val NameCategory: String,
     @ColumnInfo("name_Task")
